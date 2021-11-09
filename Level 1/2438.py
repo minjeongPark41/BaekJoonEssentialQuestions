ac35711e15d12ef09570675f244c2caed5b54377
minjeongPark41 <pmjabcd@gmail.com>
2021-11-09 13:49:34 +0900

    백준 2438번: 별 찍기1

---
+++ Level 1/2438.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+"""
+*
+**
+***
+.
+.
+
+for i in N: # 만약 N이 3이면 0, 1, 2
+-> for i in range(1, N+1) #이러면 1,2,3
+"""
+
+for i in range (1, N+1):
+    print('*' * i)
+
+    
+
+
+
